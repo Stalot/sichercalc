94b5b5e56362175faa13e4ab3baf2e05931ecb2f
Orly Neto <orly2carvalhoneto@gmail.com>
2026-02-16 23:20:17 -0300

    Depracted ast.Num changed to modern ast.Constant; README.md disclaimer; new project structure and overall organization

--- README.md
+++ README.md
@@ -1,44 +1,6 @@
-AST Calculator
+AST Calculator FORK
 ==============
 
-Simple calculator based on [Abstract syntax tree](http://en.wikipedia.org/wiki/Abstract_syntax_tree).
-In this case Python's [AST](https://docs.python.org/2/library/ast.html).
+Fork of [ast_calculator](https://github.com/mrfuxi/ast_calculator), a simple calculator based on [Abstract syntax tree](http://en.wikipedia.org/wiki/Abstract_syntax_tree). In this case Python's [AST](https://docs.python.org/2/library/ast.html).
 
-Usage
-=====
-
-Interactive mode:
-
-    ./calc.py
-
-One off evaluation from command line:
-
-    ./calc.py "1 + 2"
-
-Evaluation of all expression in the file:
-
-    ./calc.py -f input_file
-
-Help, as always:
-
-    ./calc.py --help
-
-Tests
-=====
-
-To run test execute:
-
-    python -m unittest discover
-
-Functionality
-=============
-
-Allowed
--------
-- All operators defined in ast
-- Functions: log, exp, sqrt, sin, cos
-- Const: pi, e
-
-Not allowed
------------
-- All other things ;) That included executing Python code
+_(I will think in a better name soon, I promise!)_
--- ast_calculator/main.py
+++ ast_calculator/main.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-"""
-Caclulator based on AST
-"""
-
 import ast
 import math
 import operator
-import sys
-
-from optparse import OptionParser
-
 
 def evaluate(node):
     """
@@ -86,8 +77,8 @@ def evaluate(node):
         except KeyError:
             raise ValueError("Function %s not supported" % func_name)
 
-    elif isinstance(node, ast.Num):
-        return node.n
+    elif isinstance(node, ast.Constant):
+        return node.value
 
     elif isinstance(node, ast.Name):
         try:
@@ -97,77 +88,5 @@ def evaluate(node):
 
     raise TypeError("Unsupported operation: %s" % node.__class__.__name__)
 
-
-def interactive():
-    """
-    Evaluates expressions given by user in interactive fashion
-    """
-
-    print "Please specify expressions to evaluate"
-    print "To exit please enter: empty line or word 'quit'"
-    print ""
-
-    while True:
-        expression = raw_input(">>> ")
-        if expression in ["", "quit"]:
-            print "Bye"
-            break
-
-        try:
-            print evaluate(expression)
-        except StandardError, err:
-            print "Your expression could not be evaluated:", err
-
-
-def from_file(filename):
-    """
-    Runs expressions found in given file
-    """
-
-    input_file = file(filename, "r")
-
-    for line in input_file:
-        expression = line.strip()
-        try:
-            print "%s = %s" % (expression, evaluate(expression))
-        except StandardError, err:
-            print "%s could not be evaluated: %s" % (expression, err)
-
-
-def main(argv):
-    description = "Program evaluates mathematical expressions."
-    epilog = "By default user will be prompted to input expression."
-    usage = "usage: %prog [expression | options]"
-    parser = OptionParser(description=description,
-                          epilog=epilog,
-                          usage=usage)
-
-    parser.add_option("-f", "--file", dest="filename",
-                      help="Load expressions from file")
-
-    (options, args) = parser.parse_args(argv)
-
-    if len(args) == 2:
-        # Evaluate expression from argument
-        try:
-            print evaluate(args[1])
-        except StandardError, err:
-            print "Your expression could not be evaluated:", err
-            sys.exit(1)
-
-    elif options.filename:
-        # Evaluate expressions in batch mode
-        try:
-            from_file(options.filename)
-        except IOError, err:
-            print "There was a problem with your file:", err
-            sys.exit(1)
-
-    else:
-        # Evaluate expressions entered by user
-        interactive()
-
-    sys.exit(0)
-
 if __name__ == "__main__":
-    main(sys.argv)
+    ...
