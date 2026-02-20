ca3f40ec249f52261ea3cd4d47bca01a6f93a0b8
Orly Neto <orly2carvalhoneto@gmail.com>
2026-02-20 17:03:54 -0300

    OpLogic(): Operators logic moved to it's own dedicated class; SafeEvaluator() introduced.

--- ast_calculator/main.py
+++ ast_calculator/main.py
@@ -1,92 +1,101 @@
+from typing import Any, Callable, Literal
 import ast
-import math
-import operator
+from .op_logic import OpLogic
 
-def evaluate(node):
-    """
-    Walk recursively though the mathematical expression,
-    in order to evaluate it.
+class SafeEvaluator:
+    @classmethod
+    def evaluate(cls,
+                 expression,
+                 context: None | dict[str, Any] = None,
+                 **kwargs):
+        _opLogic = OpLogic(mode=kwargs.get("mode", "float"))
+        func_map: dict[str, Callable] = {}
+        const_map: dict[str, int | float] = {}
+        
+        def context_map(context) -> None:
+            if not context:
+                return None
+            constants: None | dict[str, Literal] = context.get("const")
+            functions: None | dict[str, Callable] = context.get("func")
+            if constants:
+                for id, value in constants.items():
+                    try:
+                        ast.literal_eval(str(value))
+                        const_map.update({id: value})
+                    except ValueError as ve:
+                        raise ValueError(f"All constants must be python literals, such as str, int, float, ... But got {ve}")
+            if functions:
+                for id, func in functions.items():
+                    if not isinstance(func, Callable):
+                        raise ValueError(f"'{func}' is not callable")
+                    func_map.update({id: func})
+        def node_evaluation(node):
+            if isinstance(node, (list, tuple)):
+                return [node_evaluation(sub_node) for sub_node in node]
 
-    Valid operations are listed in op_map.
-    Valid functions are listed in func_map.
-    Valid constants are listed in const_map.
-    """
+            elif isinstance(node, str):
+                return node_evaluation(ast.parse(node))
 
-    op_map = {
-        ast.Add: operator.add,
-        ast.Sub: operator.sub,
-        ast.Mult: operator.mul,
-        ast.Div: operator.truediv,
-        ast.FloorDiv: operator.floordiv,
-        ast.Mod: operator.mod,
-        ast.Pow: operator.pow,
-        ast.LShift: operator.lshift,
-        ast.RShift: operator.rshift,
-        ast.BitOr: operator.or_,
-        ast.BitAnd: operator.and_,
-        ast.BitXor: operator.xor,
-    }
+            elif isinstance(node, ast.Module):
+                values = []
+                for body in node.body:
+                    values.append(node_evaluation(body))
+                if len(values) == 1:
+                    values = values[0]
+                return values
 
-    func_map = {
-        "log": math.log,
-        "exp": math.exp,
-        "sqrt": math.sqrt,
-        "sin": math.sin,
-        "cos": math.cos,
-    }
+            elif isinstance(node, ast.Expr):
+                return node_evaluation(node.value)
 
-    const_map = {
-        "pi": math.pi,
-        "e": math.e,
-    }
+            elif isinstance(node, ast.BinOp):
+                left = node_evaluation(node.left)
+                op = node.op
+                right = node_evaluation(node.right)
 
-    if isinstance(node, (list, tuple)):
-        return [evaluate(sub_node) for sub_node in node]
+                try:
+                    #return cls.op_map[type(op)](left, right)
+                    return _opLogic.call(type(op), left, right)
+                except KeyError:
+                    raise ValueError(
+                        "Operator %s not supported" % op.__class__.__name__)
 
-    elif isinstance(node, str):
-        return evaluate(ast.parse(node))
+            elif isinstance(node, ast.Call):
+                try:
+                    func_name = node.func.id
+                except AttributeError:
+                    raise ValueError(f"{node.func.value}: Can't call {type(node.func.value).__name__} object as a function")
+                args = [node_evaluation(arg) for arg in node.args]
 
-    elif isinstance(node, ast.Module):
-        values = []
-        for body in node.body:
-            values.append(evaluate(body))
-        if len(values) == 1:
-            values = values[0]
-        return values
+                try:
+                    return func_map[func_name](*args)
+                except KeyError:
+                    raise ValueError("Function %s not supported" % func_name)
 
-    elif isinstance(node, ast.Expr):
-        return evaluate(node.value)
+            elif isinstance(node, ast.Constant):
+                return node.value
 
-    elif isinstance(node, ast.BinOp):
-        left = evaluate(node.left)
-        op = node.op
-        right = evaluate(node.right)
+            elif isinstance(node, ast.Name):
+                try:
+                    return const_map[node.id]
+                except KeyError:
+                    raise ValueError("Constant %s not supported" % node.id)
 
-        try:
-            return op_map[type(op)](left, right)
-        except KeyError:
-            raise ValueError(
-                "Operator %s not supported" % op.__class__.__name__)
-
-    elif isinstance(node, ast.Call):
-        func_name = node.func.id
-        args = [evaluate(arg) for arg in node.args]
-
-        try:
-            return func_map[func_name](*args)
-        except KeyError:
-            raise ValueError("Function %s not supported" % func_name)
-
-    elif isinstance(node, ast.Constant):
-        return node.value
-
-    elif isinstance(node, ast.Name):
-        try:
-            return const_map[node.id]
-        except KeyError:
-            raise ValueError("Constant %s not supported" % node.id)
-
-    raise TypeError("Unsupported operation: %s" % node.__class__.__name__)
+            raise TypeError("Unsupported operation: %s" % node.__class__.__name__)
+        context_map(context)
+             
+        return node_evaluation(expression)
 
 if __name__ == "__main__":
-    ...
+    se = SafeEvaluator()
+    constants = {"aaa": "2653.0000000000000000000000000000000"}
+    def zero():
+        return 0
+    functions = {"zero": zero}
+    context = {
+        "const": constants,
+        "func": functions
+    }
+    def my_func():
+        ...
+    result = se.evaluate("2+2/1", context)
+    print(result)
---
+++ ast_calculator/op_logic.py
@@ -0,0 +1,59 @@
+from typing import Callable
+import ast
+import operator 
+from decimal import Decimal, InvalidOperation
+# Operators logic
+
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def truediv(a, b):
+    return a/b
+
+class OpLogic():
+    def __init__(self, mode="float"):
+        self.mode = mode
+    op_map = {
+        ast.Add: operator.add,
+        ast.Sub: operator.sub,
+        ast.Mult: operator.mul,
+        ast.Div: operator.truediv,
+        ast.FloorDiv: operator.floordiv,
+        ast.Mod: operator.mod,
+        ast.Pow: operator.pow,
+        ast.LShift: operator.lshift,
+        ast.RShift: operator.rshift,
+        ast.BitOr: operator.or_,
+        ast.BitAnd: operator.and_,
+        ast.BitXor: operator.xor,
+    }
+    def call(self,
+             node_op,
+             left,
+             right):
+        def convert(number: int | float):
+            match self.mode:
+                case "decimal": 
+                    try:
+                        new_number = Decimal(str(number))
+                    except InvalidOperation:
+                        raise ValueError(f"Couldn't convert {type(number).__name__} '{number}' to a Decimal object")
+                case "float":
+                    new_number = float(number)
+                case _:
+                    raise ValueError(f"'{self.mode}' mode is unknown. Valid modes are: float and decimal")
+            return new_number
+        try:
+            l: Decimal = convert(left)
+            r: Decimal = convert(right)
+            return self.op_map[node_op](l, r)
+        except KeyError:
+            raise ValueError(f"{node_op} operator not supported.")
+
+if __name__ == "__main__":
+    opLogic = OpLogic("decimal")
+    result = opLogic.call(ast.Mult, 2.722772727, 3.1483828293388383)
+    print(result)
