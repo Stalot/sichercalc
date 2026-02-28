53294cd66eb084ab6438f21f5d1a0fd2ca6948bb
Orly Neto <orly2carvalhoneto@gmail.com>
2026-02-28 14:14:23 -0300

    NodeInterpreter(): node evaluation logic moved to it's own dedicated class.

--- ast_calculator/main.py
+++ ast_calculator/main.py
@@ -1,87 +1,18 @@
 from typing import Any, Callable, Literal
 import ast
 from .op_logic import OpLogic
+from .node_interpreter import NodeInterpreter
 
 class SafeEvaluator:
-    @classmethod
-    def evaluate(cls,
-                 expression,
+    @staticmethod
+    def evaluate(expression,
                  **kwargs):
-        _opLogic = OpLogic(mode=kwargs.get("mode", "none"))
-        func_map: dict[str, Callable] = {}
-        const_map: dict[str, int | float] = {}
+        _opLogic: OpLogic = OpLogic(mode=kwargs.get("mode", "none"))
+        _inter: NodeInterpreter = NodeInterpreter()
+        _inter._opLogic = _opLogic
         
-        def context_map(context) -> None:
-            if not context:
-                return None
-            constants: None | dict[str, Literal] = context.get("const")
-            functions: None | dict[str, Callable] = context.get("func")
-            if constants:
-                for id, value in constants.items():
-                    try:
-                        ast.literal_eval(str(value))
-                        const_map.update({id: value})
-                    except ValueError as ve:
-                        raise ValueError(f"All constants must be python literals, such as str, int, float, ... But got {ve}")
-            if functions:
-                for id, func in functions.items():
-                    if not isinstance(func, Callable):
-                        raise ValueError(f"'{func}' is not callable")
-                    func_map.update({id: func})
-        def node_evaluation(node):
-            if isinstance(node, (list, tuple)):
-                return [node_evaluation(sub_node) for sub_node in node]
-
-            elif isinstance(node, str):
-                return node_evaluation(ast.parse(node))
-
-            elif isinstance(node, ast.Module):
-                values = []
-                for body in node.body:
-                    values.append(node_evaluation(body))
-                if len(values) == 1:
-                    values = values[0]
-                return values
-
-            elif isinstance(node, ast.Expr):
-                return node_evaluation(node.value)
-
-            elif isinstance(node, ast.BinOp):
-                left = node_evaluation(node.left)
-                op = node.op
-                right = node_evaluation(node.right)
-
-                try:
-                    return _opLogic.call(type(op), left, right)
-                except KeyError:
-                    raise ValueError(
-                        f"Operator {op.__class__.__name__} not supported")
-
-            elif isinstance(node, ast.Call):
-                try:
-                    func_name = node.func.id
-                except AttributeError:
-                    raise ValueError(f"{node.func.value}: Can't call {type(node.func.value).__name__} object as a function")
-                args = [node_evaluation(arg) for arg in node.args]
-
-                try:
-                    return func_map[func_name](*args)
-                except KeyError:
-                    raise ValueError(f"Function '{func_name}' not supported")
-
-            elif isinstance(node, ast.Constant):
-                return node.value
-
-            elif isinstance(node, ast.Name):
-                try:
-                    return const_map[node.id]
-                except KeyError:
-                    raise ValueError(f"Constant '{node.id}' not supported")
-
-            raise TypeError(f"Unsupported operation: {node.__class__.__name__}")
-        context_map(kwargs.get("context"))
-             
-        return node_evaluation(expression)
+        _inter.context_map(kwargs.get("context"))
+        return _inter.eval_node(expression, )
 
 if __name__ == "__main__":
     ...
---
+++ ast_calculator/node_interpreter.py
@@ -0,0 +1,94 @@
+import ast
+from typing import Any, Literal, Callable
+from .op_logic import OpLogic
+
+class NodeInterpreter:
+    _opLogic: OpLogic = OpLogic()
+
+    def _binop(self, node: ast.BinOp):
+        left = self.eval_node(node.left)
+        op = node.op
+        right = self.eval_node(node.right)
+        try:
+            return self._opLogic.call(type(op), left, right)
+        except KeyError:
+            raise ValueError(f"Operator {op.__class__.__name__} not supported")
+
+    def _string(self, node: str):
+        return self.eval_node(ast.parse(node))
+
+    def _constant(self, node: ast.Constant):
+        return node.value
+
+    def _expr(self, node: ast.Expr):
+        return self.eval_node(node.value)
+
+    def _module(self, node: ast.Module):
+        values = []
+        for body in node.body:
+            values.append(self.eval_node(body))
+            if len(values) == 1:
+                values = values[0]
+        return values
+    def _call(self, node: ast.Call):
+        try:
+            func_name = node.func.id
+        except AttributeError:
+            raise ValueError(f"{node.func.value}: Can't call {type(node.func.value).__name__} object as a function")
+   
+        try:
+            args = [self.eval_node(arg) for arg in node.args]
+            return self.func_map[func_name](*args)
+        except KeyError:
+            raise ValueError(f"Function '{func_name}' not supported")
+    def _name(self, node: ast.Name):
+        try:
+            return self.const_map[node.id]
+        except KeyError:
+            raise ValueError(f"Constant '{node.id}' not supported")
+    
+    instance_map = {
+        str: _string,
+        ast.BinOp: _binop,
+        ast.Constant: _constant,
+        ast.Expr: _expr,
+        ast.Module: _module,
+        ast.Call: _call,
+        ast.Name: _name
+    }
+    
+    func_map: dict[str, Callable] = {}
+    const_map: dict[str, int | float] = {}
+        
+    def context_map(self, context) -> None:
+        if not context:
+            return None
+        constants: None | dict[str, Literal] = context.get("const")
+        functions: None | dict[str, Callable] = context.get("func")
+        if constants:
+            for id, value in constants.items():
+                try:
+                    ast.literal_eval(str(value))
+                    self.const_map.update({id: value})
+                except ValueError as ve:
+                    raise ValueError(f"All constants must be python literals, such as str, int, float, ... But got {ve}")
+        if functions:
+            for id, func in functions.items():
+                if not isinstance(func, Callable):
+                    raise ValueError(f"'{func}' is not callable")
+            self.func_map.update({id: func})
+    
+    def eval_node(self, node: Any):
+        _type: Any = type(node)
+        #self.instance_map[_type](self, node)
+        if ast_instance := self.instance_map.get(_type):
+            return ast_instance(self, node)
+        raise KeyError(_type)
+        #return self.instance_map[_type](self, node)
+
+if __name__ == "__main__":
+    inter: NodeInterpreter = NodeInterpreter()
+    new_logic = OpLogic(mode="decimal")
+    inter._opLogic=new_logic
+    result = inter.eval_node("1.0383747*5.92838388229930")
+    print(f"{result=}")
