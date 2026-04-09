53388243742a8bf636ec1f8783be2d14faec9f8b
Orly Neto <orly2carvalhoneto@gmail.com>
2026-04-08 21:12:19 -0300

    More exceptions for arithmetic errors (such as zero division)

--- sichercalc/exceptions.py
+++ sichercalc/exceptions.py
@@ -1,3 +1,4 @@
+from typing import Any
 
 class NodeError(Exception):
     ...
@@ -5,5 +6,32 @@ class NodeError(Exception):
 class ForbiddenNode(NodeError):
     ...
 
+class BinaryOperationError(Exception):
+    ...
+
+class DivisionUndefinedError(BinaryOperationError):
+    """
+    x / 0
+    """
+    ...
+
+class DivisionIndeterminateError(BinaryOperationError):
+    """
+    0 / 0
+    """
+    ...
+
 if __name__ == "__main__":
-    raise ForbiddenNode("ast.If is forbidden")
+    binop: tuple[int, Any, int] = (0, "ast.Div", 0)
+    try:
+        raise BinaryOperationError("")
+    except BinaryOperationError:
+        l: int = binop[0]
+        op: Any = binop[1]
+        r: int = binop[2]
+        if op == "ast.Div":
+            print(l, r)
+            if l == 0 and r == 0:
+                raise DivisionIndeterminateError("Zero divided by zero is indeterminated")
+            elif l != 0 and r == 0:
+                raise DivisionUndefinedError("A non-zero number divided by zero is undefined")
