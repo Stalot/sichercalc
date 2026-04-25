d6496401e9fb51556d422c6d2a611ad83cf9010f
Orly Neto <orly2carvalhoneto@gmail.com>
2026-04-25 17:29:32 -0300

    New decimal context configuration added to evaluate() (quantize, rounding and normalize); new trunc method added to AstEvaluator

--- sichercalc/main.py
+++ sichercalc/main.py
@@ -1,21 +1,51 @@
 from typing import Any
 from .node_interpreter import NodeInterpreter
 import ast
+from decimal import Decimal, localcontext, ROUND_HALF_UP
 
 class AstEvaluator:
     def __init__(self):
         self._inter: NodeInterpreter = NodeInterpreter()
 
     def evaluate(self,
-                 expression,
-                 context: None | dict[str, dict[str, Any]] = None) -> str:
+                 expression: str,
+                 context: None | dict[str, dict[str, Any]] = None,
+                 quantize: None | str = "0.000001",
+                 rounding: None | str = ROUND_HALF_UP,
+                 normalize: bool = True,) -> str:
         # Clears the previous context
         self._inter.clear_context_map()
 
         if context:
-            # Local context:
             self._inter.context_map(context)
-        return str(self._inter.eval_node(expression))
+        evaluated_result: Decimal = self._inter.eval_node(expression)
+
+        # Quantizes the result after evaluation, it doesn't apply to internal calculation.
+        if quantize:
+            if not isinstance(quantize, str):
+                raise TypeError(f"quantize must be a string, not {type(quantize)!r}")
+            evaluated_result = evaluated_result.quantize(Decimal(quantize),
+                                      rounding=rounding)
+        # Normalizes the result after evaluation, it doesn't apply to internal calculation.
+        if normalize:
+            if not isinstance(normalize, bool):
+                raise TypeError(f"normalize must be a boolean, not {type(normalize)!r}")
+            evaluated_result = evaluated_result.normalize()
+        return str(evaluated_result)
+
+    def trunc(self,
+              x: Any,
+              places: int) -> str:
+        """
+        Truncates a number to a specific number of decimal places.
+        """
+        number: str = str(x)
+        decimal_places: int = int(places)
+        if period := number.index("."):
+            trunc = number[period:period+1+decimal_places]
+            number = number[:period] + trunc
+        return number
+
 
 if __name__ == "__main__":
     ...
