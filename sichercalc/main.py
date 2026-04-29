2c7e2faa2c68d2576d7be5090a464ff2e482a04a
Orly Neto <orly2carvalhoneto@gmail.com>
2026-04-29 19:02:46 -0300

    normalize arg removed from evaluate; enhancements in trunc() and evaluate() to prevent trailing zeros.

--- sichercalc/main.py
+++ sichercalc/main.py
@@ -10,9 +10,8 @@ class AstEvaluator:
     def evaluate(self,
                  expression: str,
                  context: None | dict[str, dict[str, Any]] = None,
-                 quantize: None | str = "0.000001",
-                 rounding: None | str = ROUND_HALF_UP,
-                 normalize: bool = True,) -> str:
+                 quantize: None | str = "0.000000",
+                 rounding: None | str = ROUND_HALF_UP) -> str:
         # Clears the previous context
         self._inter.clear_context_map()
 
@@ -26,28 +25,24 @@ class AstEvaluator:
                 raise TypeError(f"quantize must be a string, not {type(quantize)!r}")
             evaluated_result = evaluated_result.quantize(Decimal(quantize),
                                                          rounding=rounding)
-        # Normalizes the result after evaluation, it doesn't apply to internal calculation.
-        if normalize:
-            if not isinstance(normalize, bool):
-                raise TypeError(f"normalize must be a boolean, not {type(normalize)!r}")
-            evaluated_result = evaluated_result.normalize()
-        return str(evaluated_result)
+        as_string = f"{evaluated_result:f}"
+        return as_string
 
     def trunc(self,
               x: Any,
-              places: int) -> str:
+              prec: int) -> str:
         """
         Truncates a number to a specific number of decimal places.
         """
         number: str = str(x)
-        decimal_places: int = int(places)
+        decimal_places: int = int(prec)
         period: int = number.find(".") # returns -1 if not found
         if period != -1:
             trunc: str = number[period:period+1+decimal_places]
             number: str = number[:period] + trunc
             # removes trailing zeros
             if trunc.endswith("0"):
-                number = str(Decimal(number).normalize())
+                number = number.rstrip("0").rstrip(".")
         return number
 
 
